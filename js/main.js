c6e70255c59c737cc571d22f3eb1d4cff39ae960
fumio68 <rgkk.5123@gmail.com>
2020-03-31 00:29:23 +0900

    lesson5-5(スライドショー)

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
 'use strict';
 
 {
-  const thumbs = document.querySelectorAll('.thumb');
-  thumbs.forEach(function(item, index) {
-    item.onclick = function() {
-      document.getElementById('bigimg').src = this.dataset.image;
+  const images = ['img/5-5/image1.jpg', 'img/5-5/image2.jpg', 'img/5-5/image3.jpg', 'img/5-5/image4.jpg','img/5-5/image5.jpg'];
+  let current = 0;
+
+  function changeImage(num) {
+    if(current + num >= 0 && current + num < images.length){
+      current += num;
+      document.getElementById('main_image').src = images[current];
+      pageNum();
     }
-  });
+  };
+
+  function pageNum() {
+    document.getElementById('page').textContent = `${current + 1}/${images.length}`;
+  }
+
+  pageNum();
+
+  document.getElementById('prev').onclick = function() {
+    changeImage(-1);
+  };
+
+  document.getElementById('next').onclick = function() {
+    changeImage(1);
+  };
 }
