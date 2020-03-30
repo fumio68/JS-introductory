014ed242306edfbbf352829935ae295a8c9b06bc
fumio68 <rgkk.5123@gmail.com>
2020-03-30 14:46:51 +0900

    lesson4-1(フォームの値習得)

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 'use strict';
 
 {
-  let jsbook = {title: 'JavaScript入門', price: 2500, stock: 3};
-  document.getElementById('title').textContent = jsbook.title;
-  document.getElementById('price').textContent = jsbook.price;
-  document.getElementById('stock').textContent = jsbook.stock;
+  document.getElementById('form').onsubmit = function(event) {
+    event.preventDefault();
+    const search = document.getElementById('form').word.value;
+    document.getElementById('output').textContent = `「${search}」の検索中・・・`
+  };
 }
