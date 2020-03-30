2fdd09a9963f1e24b6d741b7804beb0d5edc2707
fumio68 <rgkk.5123@gmail.com>
2020-03-30 16:30:29 +0900

    lesson4-3(小数点の計算)

--- js/main.js
+++ js/main.js
@@ -1,19 +1,12 @@
 'use strict';
 
 {
-  const now = new Date();
-  const year = now.getFullYear();
-  const month = ("0" + (now.getMonth() + 1)).slice(-2);
-  const date = ("0" + now.getDate()).slice(-2);
-  const hour = ("0" + now.getHours()).slice(-2);
-  const min = ("0" + now.getMinutes()).slice(-2);
-  let ampm = '';
-  if(hour < 12) {
-    ampm = 'a.m.';
-  }else {
-    ampm = 'p.m.';
-  }
+  document.getElementById('p1').textContent = Math.PI;
+  document.getElementById('floor').textContent = Math.floor(Math.PI);
 
-  const output = `${year}/${month}/${date} ${hour % 12}:${min}${ampm}`;
-  document.getElementById('time').textContent = output;
+  function point(num, digit) {
+    const mover = 10 ** digit;
+    return Math.floor(num * mover) / mover;
+  }
+  document.getElementById('output').textContent = point(Math.PI, 2);
 }
