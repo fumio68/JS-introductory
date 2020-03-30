88ea15b8208fbc05393b0206a70adfbdeda50e9d
fumio68 <rgkk.5123@gmail.com>
2020-03-30 20:13:42 +0900

    lesson5-2(プルダウンメニューでページ移動)

--- js/main.js
+++ js/main.js
@@ -1,33 +1,29 @@
 'use strict';
 
 {
-  function countdown(due) {
-    const now = new Date();
+  const lang = document.querySelector('html').lang;
 
-    const rest = due.getTime() - now.getTime();
-    const sec = Math.floor(rest / 1000) % 60;
-    const min = Math.floor(rest / 1000 / 60) % 60;
-    const hours = Math.floor(rest / 1000 / 60 / 60) % 24;
-    const days = Math.floor(rest / 1000 / 60 / 60 / 24);
-    const count = [days,hours ,min ,sec];
+  // if(lang === 'ja') {
+  //   document.querySelector('option[value="index.html"]').selected = true;
+  // } else if(lang === 'en') {
+  //   document.querySelector('option[value="index-en.html"]').selected = true;
+  // } else if(lang === 'zh') {
+  //   document.querySelector('option[value="index-zh.html"]').selected = true;
+  // }
 
-    return count;
+  switch(lang) {
+    case 'ja':
+      document.querySelector('option[value="index.html"]').selected = true;
+    break;
+    case 'en':
+      document.querySelector('option[value="index-en.html"]').selected = true;
+    break;
+    case 'zh':
+      document.querySelector('option[value="index-zh.html"]').selected = true;
+    break;
   }
 
-  let goal = new Date(2025, 4, 3);
-
-  function recalc() {
-    const counter = countdown(goal);
-    document.getElementById('day').textContent = counter[0];
-    document.getElementById('hour').textContent = String(counter[1]).padStart(2, '0');
-    document.getElementById('min').textContent = String(counter[2]).padStart(2, '0');
-    document.getElementById('sec').textContent = String(counter[3]).padStart(2, '0');
-    refresh();
+  document.getElementById('form').select.onchange = function() {
+    location.href = document.getElementById('form').select.value;
   }
-
-  function refresh() {
-    setTimeout(recalc, 1000);
-  }
-
-  recalc();
 }
