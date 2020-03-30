29c01e79a1db0c4d4b0eafccf1f4896360654350
fumio68 <rgkk.5123@gmail.com>
2020-03-30 15:39:29 +0900

    lesson4-2(見やすい時間の表示)

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 'use strict';
 
 {
-  document.getElementById('form').onsubmit = function(event) {
-    event.preventDefault();
-    const search = document.getElementById('form').word.value;
-    document.getElementById('output').textContent = `「${search}」の検索中・・・`
-  };
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = ("0" + (now.getMonth() + 1)).slice(-2);
+  const date = ("0" + now.getDate()).slice(-2);
+  const hour = ("0" + now.getHours()).slice(-2);
+  const min = ("0" + now.getMinutes()).slice(-2);
+  let ampm = '';
+  if(hour < 12) {
+    ampm = 'a.m.';
+  }else {
+    ampm = 'p.m.';
+  }
+
+  const output = `${year}/${month}/${date} ${hour % 12}:${min}${ampm}`;
+  document.getElementById('time').textContent = output;
 }
