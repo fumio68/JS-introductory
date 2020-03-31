26572ef263147dc66a553bdbc44736df25f749a1
fumio68 <rgkk.5123@gmail.com>
2020-03-31 22:35:05 +0900

    lesson6-3(Ajax通信)

--- js/main.js
+++ js/main.js
@@ -5,11 +5,22 @@
     $.ajax({url: 'data.json', dataType: 'json'})
     .done(function(data){
       data.forEach(function(item, index){
-        console.log(item);
+        if(item.crowded === 'yes') {
+          const idName = '#' + item.id;
+          $(idName).find('.check').addClass('crowded');
+        }
       });
     })
     .fail(function(){
       window.alert('読み込みエラー');
     });
+  });
+
+  $('.check').on('click', function(){
+    if($(this).hasClass('crowded')){
+      $(this).text('残席わずか').addClass('red');
+    } else {
+      $(this).text('お席あります').addClass('green');
+    }
   });
 }
